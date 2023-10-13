ecd906760b59df6d5cdd1a18ec7de6029391a3a0
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2023-10-13 15:53:47 +0300

    added fixtures

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,20 +1,43 @@
+import pytest
+
 from utils import arrs
 from utils import dicts
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
-    assert arrs.get([], 0, "test") == "test"
+@pytest.fixture
+def ex_list():
+    return [1, 2, 3, 4]
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], None, 2) == [1, 2]
-    assert arrs.my_slice([]) == []
+@pytest.fixture
+def empty_list():
+    return []
 
 
-def test_get_val():
-    assert dicts.get_val({1: "bird", 2: "fish", 3: "cat"}, 1) == "bird"
-    assert dicts.get_val({1: "bird", 2: "fish", 3: "cat"}, 0) == "git"
-    assert dicts.get_val({1: "bird", 2: "fish", 3: "cat"}, 0, "not found") == "not found"
+@pytest.fixture
+def ex_dict():
+    return {1: "bird", 2: "fish", 3: "cat"}
+
+
+def test_get(ex_list):
+    assert arrs.get(ex_list, 1, "test") == 2
+
+
+def test_get_empty(empty_list):
+    assert arrs.get(empty_list, 0, "test") == "test"
+
+
+def test_slice(ex_list):
+    assert arrs.my_slice(ex_list, 1, 3) == [2, 3]
+    assert arrs.my_slice(ex_list, 1) == [2, 3, 4]
+    assert arrs.my_slice(ex_list, None, 2) == [1, 2]
+
+
+def test_slice_empty(empty_list):
+    assert arrs.my_slice(empty_list) == []
+
+
+def test_get_val(ex_dict):
+    assert dicts.get_val(ex_dict, 1) == "bird"
+    assert dicts.get_val(ex_dict, 0) == "git"
+    assert dicts.get_val(ex_dict, 0, "not found") == "not found"
